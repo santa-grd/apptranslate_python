5e831355552a2c372d2d8344c086f8faa92b65c1
geri_ramadansyah <137025601+santa-grd@users.noreply.github.com>
2024-11-08 01:54:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ function showAllSites() {
     const fogEffect = document.querySelector(".fog-effect");
     const showMoreBtn = document.getElementById("showMoreBtn");
 
-    // Cek apakah tinggi kontainer sudah terbuka penuh
     if (siteContainerWrapper.style.maxHeight === "none") {
-        // Mengembalikan ke tampilan awal dengan batas tinggi
+ 
         siteContainerWrapper.style.maxHeight = "200px";
-        fogEffect.style.display = "block"; // Menampilkan kabut
-        showMoreBtn.textContent = "Tampilkan Semua Situs"; // Ubah teks tombol
+        fogEffect.style.display = "block"; 
+        showMoreBtn.textContent = "Tampilkan Semua Situs"; 
     } else {
-        // Menghapus batas tinggi untuk menampilkan semua situs
         siteContainerWrapper.style.maxHeight = "none";
-        fogEffect.style.display = "none"; // Hilangkan kabut
-        showMoreBtn.textContent = "Sembunyikan"; // Ubah teks tombol
+        fogEffect.style.display = "none";
+        showMoreBtn.textContent = "Sembunyikan"; 
     }
 }
